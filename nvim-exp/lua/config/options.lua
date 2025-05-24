fef2ecfd7734a735d2a04ae24daff5409b1cd3a5
SumOfAllN00bs <SumOfAllN00bs@users.noreply.github.com>
2025-05-25 07:26:35 +1200

    Arg real monsters
    Post phase 6 updates.
    Many changes.
    Things were done.
    Good things? I don't know.

--- nvim-exp/lua/config/options.lua
+++ nvim-exp/lua/config/options.lua
@@ -1,27 +1,29 @@
 vim.g.have_nerd_font = true
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('n', '<leader><leader>', 'gg=G', { desc = 'Reindent whole file' })
+vim.g.python3_host_prog = "/usr/sbin/python3"
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader><leader>", "gg=G", { desc = "Reindent whole file" })
 vim.opt.autoindent = true
 vim.opt.breakindent = true
 vim.opt.confirm = true
 vim.opt.cursorline = true
 vim.opt.expandtab = true
 vim.opt.ignorecase = true
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.mouse = 'a'
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.mouse = "a"
 vim.opt.number = true
+vim.opt.foldenable = false
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
 vim.opt.shiftwidth = 4
 vim.opt.showmode = false
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.smartcase = true
 vim.opt.smartindent = true
 vim.opt.smarttab = true
@@ -31,13 +33,53 @@ vim.opt.splitright = true
 vim.opt.tabstop = 4
 vim.opt.undofile = true
 vim.opt.updatetime = 200
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
     callback = function()
         vim.highlight.on_yank()
     end,
 })
 vim.schedule(function()
-    vim.opt.clipboard = 'unnamedplus'
+    vim.opt.clipboard = "unnamedplus"
 end)
+vim.g.firenvim_config = {
+    globalSettings = { alt = "all" },
+    localSettings = {
+        [".*"] = {
+            cmdline = "neovim",
+            content = "text",
+            priority = 0,
+            selector = "textarea",
+            takeover = "never",
+        },
+    },
+}
+if vim.g.started_by_firenvim then
+    vim.opt.autowrite = true
+    vim.api.nvim_create_autocmd("InsertLeave", {
+        callback = function()
+            vim.cmd("w")
+        end,
+    })
+    vim.api.nvim_create_autocmd("UIEnter", {
+        callback = function()
+            vim.fn.timer_start(1000, function()
+                if vim.api.nvim_get_option("lines") < 10 then
+                    vim.opt.lines = 10
+                end
+                vim.cmd("startinsert")
+            end)
+        end,
+    })
+    vim.api.nvim_create_user_command("SubmitForm", function()
+        vim.defer_fn(function()
+            vim.cmd("w")
+            vim.fn["firenvim#press_keys"]("<CR>")
+            vim.fn["firenvim#hide_frame"]()
+        end, 500)
+    end, {})
+    vim.api.nvim_set_keymap("n", "<C-CR>", ":SubmitForm<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("i", "<C-CR>", "<Esc>:SubmitForm<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<Esc><Esc>", "<Cmd>call firenvim#focus_page()<CR>", {})
+end
--- nvim-exp/lua/plugins/nvim-treesitter.lua
+++ nvim-exp/lua/plugins/nvim-treesitter.lua
@@ -8,7 +8,7 @@ return {
             ensure_installed = { "asm", "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "python", "bash", "yaml", "json" },
             auto_install = true,
             sync_install = false,
-            highlight = { enable = true },
+            highlight = { enable = true, additional_vim_regex_highlighting = false, },
             indent = { enable = true },
             incremental_selection = {
                 enable = true,
